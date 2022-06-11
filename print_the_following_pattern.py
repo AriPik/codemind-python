e2b2aeba76d4d90645c8f4f6a77532335271f9f3
Arijit Das <97512141+AriPik@users.noreply.github.com>
2022-06-11 20:39:17 +0530

    size = int(input())
    for i in range(size):
        for j in range(size):
            print(chr(65 + i), end=" ")
        print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end='')
+size = int(input())
+
+for i in range(size):
+    for j in range(size):
+        print(chr(65 + i), end=" ")
     print()
