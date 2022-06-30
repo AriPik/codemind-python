05f76ae114e1143ac9088d1abb47d420584f7077
Arijit Das <97512141+AriPik@users.noreply.github.com>
2022-06-30 17:28:09 +0530

    def pal(n):     temp=n     s=0     while(n!=0):         d=n%10         s=s*10+d         n=n//10     if(s==temp):         return 1     else:         return 0 n=int(input()) arr=list(map(int,input().split())) c=0 for i in range(len(arr)):     if(pal(arr[i])):         c=c+1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,18 +1,18 @@
-def palindrome(n):
-    x=n
-    rev=0
+def pal(n):
+    temp=n
+    s=0
     while(n!=0):
-        rem=n%10
-        rev=rev*10+rem
+        d=n%10
+        s=s*10+d
         n=n//10
-    if x==rev:
+    if(s==temp):
         return 1
     else:
         return 0
 n=int(input())
-a=list(map(int,input().split()))[:n]
+arr=list(map(int,input().split()))
 c=0
-for i in range(len(a)):
-    if palindrome(a[i]):
-        c+=1
-print(c)
+for i in range(len(arr)):
+    if(pal(arr[i])):
+        c=c+1
+print(c)        
