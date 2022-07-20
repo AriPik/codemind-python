90f4f26f0ad6a633786eddd88175e6b10e619b99
Arijit Das <97512141+AriPik@users.noreply.github.com>
2022-07-20 11:00:07 +0530

    s=input().lower() a=s.split() c1=0 v=['a','e','i','o','u'] c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'] for i in a:     l=len(i)     if i[0] in v and i[l-1] in c:         c1+=1 print(c1)

--- count_words.py
+++ count_words.py
@@ -1,13 +1,10 @@
-s=input()
-s=s.lower()
-n=s.split()
-l=len(n)
-x=0
+s=input().lower()
+a=s.split()
+c1=0
 v=['a','e','i','o','u']
 c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-for i in range(0,l):
-    k=n[i]
-    s1=len(k)
-    if k[0] in v and k[s1-1] in c:
-        x+=1
-print(x)
+for i in a:
+    l=len(i)
+    if i[0] in v and i[l-1] in c:
+        c1+=1
+print(c1)
