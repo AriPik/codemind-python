98cf3598bbeb15305db25100123830c43ad6e6e3
Arijit Das <97512141+AriPik@users.noreply.github.com>
2022-07-21 11:08:01 +0530

    s1=input().lower().replace(' ',"") s2=input().lower().replace(' ',"") c="" d=1 for i in s1:     if i not in s2:         c+=i for i in s2:     if i not in s1:         c+=i s=list(set(c)) print(len(s))

---
+++ Non_common_characters.py
@@ -0,0 +1,12 @@
+s1=input().lower().replace(' ',"")
+s2=input().lower().replace(' ',"")
+c=""
+d=1
+for i in s1:
+    if i not in s2:
+        c+=i
+for i in s2:
+    if i not in s1:
+        c+=i
+s=list(set(c))
+print(len(s))
